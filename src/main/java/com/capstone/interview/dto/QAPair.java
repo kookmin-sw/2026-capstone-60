c06d5f6f35bd3b04acc28c8cdd20c09102da5bb1
Silbet <gs1237@kookmin.ac.kr>
2026-04-22 05:17:11 +0900

    refactor: 구 스키마 기반 DTO 6개 삭제

--- src/main/java/com/capstone/interview/dto/QAPair.java
+++
@@ -1,9 +0,0 @@
-package com.capstone.interview.dto;
-
-/** 질문-답변 쌍. ConversationContext의 history에 누적됨 */
-public record QAPair(
-    String questionId,
-    String questionText,
-    String questionType,
-    String answerText
-) {}
