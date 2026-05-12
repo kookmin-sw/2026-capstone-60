a1912d4e857a3f13b1634c668ffb24350d0e0cab
eunmiii <163962545+eunmiii@users.noreply.github.com>
2026-05-12 19:24:11 +0900

    Reorganize InterviewQnaRepository methods

--- src/main/java/com/capstone/interview/repository/InterviewQnaRepository.java
+++ src/main/java/com/capstone/interview/repository/InterviewQnaRepository.java
@@ -6,15 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface InterviewQnaRepository extends JpaRepository<InterviewQna, Long> {
-
-    // 면접의 모든 질문-답변을 순서대로 조회
-    List<InterviewQna> findByInterviewOrderBySequenceNumberAsc(Interview interview);
-}
 import java.util.Optional;
 
 public interface InterviewQnaRepository extends JpaRepository<InterviewQna, Long> {
-
+    // 면접의 모든 질문-답변을 순서대로 조회
+    List<InterviewQna> findByInterviewOrderBySequenceNumberAsc(Interview interview);
+    
     Optional<InterviewQna> findByInterviewAndSequenceNumber(Interview interview, Integer sequenceNumber);
 
     int countByInterview(Interview interview);
