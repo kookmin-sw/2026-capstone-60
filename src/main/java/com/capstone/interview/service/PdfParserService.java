85bd7e8efcbcf8cc73bf35eaae331286e7b54924
HTW01 <twhahm01@kookmin.ac.kr>
2026-05-13 09:16:25 +0900

    fix: PDFBox 3.x Loader.loadPDF(byte[]) 호환 수정

--- src/main/java/com/capstone/interview/service/PdfParserService.java
+++ src/main/java/com/capstone/interview/service/PdfParserService.java
@@ -1,19 +1,17 @@
 package com.capstone.interview.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import org.apache.pdfbox.Loader; // 머지 오류 해결
-
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * PDF 파일을 텍스트로 변환하는 서비스.
- * Apache PDFBox를 사용한다.
+ * Apache PDFBox 3.x를 사용한다.
  */
 @Slf4j
 @Service
@@ -21,17 +19,15 @@ public class PdfParserService {
 
     /**
      * 업로드된 PDF 파일에서 텍스트를 추출한다.
+     * PDFBox 3.x에서는 Loader.loadPDF(byte[])를 사용해야 한다.
      *
      * @param file 업로드된 PDF 파일
      * @return 추출된 텍스트
      * @throws IOException PDF 읽기 실패 시
      */
     public String extractText(MultipartFile file) throws IOException {
-        try (InputStream inputStream = file.getInputStream();
-             
-             // load를 Loader.loadPDF로!
-            PDDocument document = Loader.loadPDF(inputStream)) {
-            // PDDocument document = PDDocument.load(inputStream)) {
+        byte[] bytes = file.getBytes();
+        try (PDDocument document = Loader.loadPDF(bytes)) {
 
             PDFTextStripper stripper = new PDFTextStripper();
             String text = stripper.getText(document);
